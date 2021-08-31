28e6975e925c980de5084032eddba3a18fcd9b37
André De Bem <andrebemferraz@gmail.com>
2021-08-31 20:32:20 -0300

    Request II and code updates

--- talkerRouter.js
+++ talkerRouter.js
@@ -4,18 +4,40 @@ const express = require('express');
 
 const router = express.Router();
 
-const talkers = './talker.json';
+const talkersLocation = './talker.json';
 
-router.get('/', async (req, res, next) => {
+async function readTalkers(req, res, next) {
   try {
-    const readTalkers = await fs.readFile(talkers);
-    res.status(200).json(JSON.parse(readTalkers));
+    const file = await fs.readFile(talkersLocation);
+
+    req.talkers = JSON.parse(file);
+
+    next();
   } catch (error) {
     next({
       status: 500,
       message: error,
     });
   }
+}
+
+router.get('/', readTalkers, (req, res) => {
+  const { talkers } = req;
+
+  // if (!talkers) return next({ status: 500, message: 'Error searching talkers!' });
+  
+  res.status(200).json(talkers);
+});
+
+router.get('/:id', readTalkers, (req, res, next) => {
+  const { id } = req.params;
+  const { talkers } = req;
+  
+  const findTalker = talkers.find((talker) => talker.id === parseInt(id, 10));
+
+  if (!findTalker) return next({ status: 404, message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(findTalker);
 });
 
 module.exports = router;
