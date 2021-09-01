bb89eaef4ae21eb8f94c672c4ab881debe6e8028
André De Bem <andrebemferraz@gmail.com>
2021-09-01 19:50:46 -0300

    Request IV

--- talkerRouter.js
+++ talkerRouter.js
@@ -8,7 +8,7 @@ async function readTalkers(req, res, next) {
   try {
     const file = await fs.readFile(talkerLocation);
 
-    req.talkers = JSON.parse(file);
+    req.arrayTalkers = JSON.parse(file);
 
     next();
   } catch (error) {
@@ -19,12 +19,100 @@ async function readTalkers(req, res, next) {
   }
 }
 
-router.get('/', readTalkers, (req, res) => {
-  const { talkers } = req;
+async function overwriteTalker(req, res, next) {
+  try {
+    const { newArrayTalkers } = req;
+    const newFile = JSON.stringify(newArrayTalkers);
 
-  // if (!talkers) return next({ status: 500, message: 'Error searching talkers!' });
+    await fs.writeFile('./talker.json', newFile, 'utf-8');
+
+    next();
+  } catch (error) {
+    next({
+      status: 500,
+      message: error,
+    });
+  }
+}
+
+function validateToken(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) return next({ status: 401, message: 'Token não encontrado' });
+
+  if (token.length < 16) return next({ status: 401, message: 'Token inválido' });
+
+  next();
+}
+
+function validateNameAndAge(req, res, next) {
+    const { name, age } = req.body;
+
+    if (!name) return next({ status: 400, message: 'O campo "name" é obrigatório' });
+    if (name.length < 3) {
+      return next({ status: 400, message: 'O "name" deve ter pelo menos 3 caracteres' });
+    }
+
+    if (!age) return next({ status: 400, message: 'O campo "age" é obrigatório' });
+    if (age < 18) {
+      return next({ status: 400, message: 'A pessoa palestrante deve ser maior de idade' });
+    }
+
+    next();
+}
+
+function validateTalk(req, res, next) {
+  const { talk } = req.body;
+
+  if (!talk || !talk.watchedAt || !talk.rate) {
+    return next({
+      status: 400,
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
   
-  res.status(200).json(talkers);
+  next();
+}
+
+function validateWatchedAtAndRate(req, res, next) {
+  const { talk: { watchedAt, rate } } = req.body;
+  
+  const dateFormat = /^(0?[1-9]|[12][0-9]|3[01])[/](0?[1-9]|1[012])[/]\d{4}$/;
+  if (dateFormat.test(watchedAt) === false) {
+    return next({ status: 400, message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  if (rate > 5 || rate < 1) {
+    return next({ status: 400, message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
+}
+
+router.get('/', readTalkers, (req, res) => {
+  const { arrayTalkers } = req;
+  
+  res.status(200).json(arrayTalkers);
+});
+
+router.post('/', validateToken, validateNameAndAge, validateTalk,
+validateWatchedAtAndRate, readTalkers, (req, res, next) => {
+  const { arrayTalkers } = req;
+  const newTalker = req.body;
+
+  newTalker.id = arrayTalkers.length + 1;
+
+  arrayTalkers.push(newTalker);
+
+  req.newArrayTalker = arrayTalkers;
+
+  next();
+}, overwriteTalker, readTalkers, (req, res) => {
+  const { arrayTalkers } = req;
+
+  const addedTalker = arrayTalkers.at(-1);
+
+  res.status(201).json(addedTalker);
 });
 
 router.get('/:id', readTalkers, (req, res, next) => {
